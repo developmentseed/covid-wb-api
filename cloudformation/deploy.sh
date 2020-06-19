3e43c7977227221237d367d02f6a5a21a66ff644
Sajjad Anwar <sajjadkm@gmail.com>
2020-06-19 17:00:42 +0530

    working cloudformation with rds, loadbalancer and a task

---
+++ cloudformation/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# automatically export all variables
+set -a
+source .env
+set +a
+
+aws cloudformation deploy --template-file cloudformation.yaml --stack-name \
+$STACK_NAME --tags Project=$PROJECT --parameter-overrides \
+DBUser=$DBUser DBPassword=$DBPassword \
+DBName=$DBName DBUser=$DBUser DBPassword=$DBPassword \
+DesiredCount=$DesiredCount \
+--region $REGION --capabilities CAPABILITY_IAM \
