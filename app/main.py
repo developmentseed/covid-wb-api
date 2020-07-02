6d582aac12d38a538b7a3252c70fd2a1be1cf9e2
David Bitner <bitner@dbspatial.com>
2020-07-02 16:21:00 -0500

    make minimal build of API elements

--- app/main.py
+++ app/main.py
@@ -1,19 +1,19 @@
 import os
+import json
 from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.openapi.utils import get_openapi
-from .routers import pygeoapi_router
-from timvt.endpoints import tiles, tms
+from timvt.endpoints import tiles, tms, index, demo
 from timvt.events import create_start_app_handler, create_stop_app_handler
-import pygeoapi
 import logging
 import sys
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
-app = FastAPI()
+app = FastAPI(docs_url="/")
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 app.add_middleware(GZipMiddleware, minimum_size=0)
 
@@ -21,69 +21,58 @@ app.add_event_handler("startup", create_start_app_handler(app))
 app.add_event_handler("shutdown", create_stop_app_handler(app))
 
 app.include_router(
-    pygeoapi_router.router, prefix="/pygeoapi",
-)
-
-app.include_router(
-    tiles.router, prefix="/timvt",
+    demo.router, prefix="/vector",
 )
 app.include_router(
-    tms.router, prefix="/timvt",
+    tiles.router, prefix="/vector",
 )
+# remove "/tiles/{identifier}/{table}/{z}/{x}/{y}.pbf" endpoint
+for r in app.routes:
+    if r.path == '/vector/tiles/{identifier}/{table}/{z}/{x}/{y}.pbf':
+        app.routes.remove(r)
+    if r.path == '/vector/':
+        app.routes.remove(r)
 
-app.mount(
-    "/pygeoapi/static",
-    StaticFiles(directory=os.path.join(pygeoapi.__path__[0], "static")),
-)
 
 
-def merge(source, destination):
-    for key, value in source.items():
-        if isinstance(value, dict):
-            # get node or create one
-            node = destination.setdefault(key, {})
-            merge(value, node)
-        else:
-            destination[key] = value
+@app.get("/RiskSchema.json", tags=["Risk Schema"], summary="Risk Schema")
+async def root(request: Request) -> JSONResponse:
+    with open('app/templates/RiskSchema.json','r') as f:
+        content = json.loads(f.read())
 
-    return destination
+    response = JSONResponse(content=content, status_code=200)
+
+    return response
 
 
 def custom_openapi(openapi_prefix: str):
     if app.openapi_schema:
         return app.openapi_schema
-    openapi_schema = get_openapi(
+    o = get_openapi(
         title="World Bank Covid API",
         version="0.1",
         description="API for World Bank Covid 19 Project",
         routes=app.routes,
         openapi_prefix=openapi_prefix,
     )
-    p = pygeoapi_router.openapiobj().copy()
-    p["paths"] = {f"/pygeoapi{k}": v for k, v in p["paths"].items()}
-    del p["servers"]
-    merged = merge(openapi_schema, p)
 
-    app.openapi_schema = merged
+    cat = app.state.Catalog
+    tables_schema = {
+        'title' : 'Table',
+        'enum' : [r["table"] for r in cat.index]
+    }
+    #raise Exception(o['paths'].keys())
+    for path in o['paths'].values():
+        if path['get']['summary'] == 'Demo':
+            path['get']['summary'] = "Vector Tile Simple Viewer"
+            path['get']['tags'] = ["Vector Tile Simple Viewer"]
+        parameters = path['get'].get('parameters')
+        if parameters is not None:
+            for param in parameters:
+                if param.get('description') == "Table Name":
+                    param['schema'] = tables_schema
+    app.openapi_schema = o
     return app.openapi_schema
 
 
 app.openapi = custom_openapi
-
-
-@app.route("/")
-async def root(request: Request) -> Response:
-    """
-    HTTP root content of Covid WB. Intro page access point
-    :returns: Starlette HTTP Response
-    """
-    content = """
-    <html><body>
-    <ul>
-    <li><a href='pygeoapi/'>PyGeoAPI Entrypoint</a></li>
-    <li><a href='docs'>OpenAPI Docs</a></li>
-    </body></html>"""
-
-    response = Response(content=content, status_code=200)
-
-    return response
